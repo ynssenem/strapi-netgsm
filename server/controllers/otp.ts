0a1d0848f416cf9ef8b9238659f81d76ff01ea81
Yunus SENEM <yunussenem@icloud.com>
2023-12-24 00:12:06 +0300

    Bulk version

--- server/controllers/otp.ts
+++ server/controllers/otp.ts
@@ -1,112 +1,105 @@
-import { Context } from "koa";
-import validations from "../utils/validations";
-import code from "../utils/code";
-import access from "../utils/access";
-import { errors } from "@strapi/utils";
-import Netgsm from "@ynssenem/netgsm";
+import validations from "../utils/validations"
+import code from "../utils/code"
+import access from "../utils/access"
+import { errors } from "@strapi/utils"
+import Netgsm from "@ynssenem/netgsm"
 
 type PhoneProps = {
-  phoneAreaCode: string;
-  phoneNumber: string;
-};
+  phoneAreaCode: string
+  phoneNumber: string
+}
 
 type CreatePhoneVerificationBodyProps = PhoneProps & {
-  message: string;
-};
+  message: string
+}
 
 type PhoneVerificationBodyProps = PhoneProps & {
-  code: string;
-};
+  code: string
+}
 
 const trim = (value: string) => {
-  const pattern = /[^0-9]/g;
-  return value.replace(pattern, "");
-};
+  const pattern = /[^0-9]/g
+  return value.replace(pattern, "")
+}
 
 export default ({ strapi }) => ({
   createPhoneVerification: async (ctx) => {
-    const params = ctx.request.body as CreatePhoneVerificationBodyProps;
+    const params = ctx.request.body as CreatePhoneVerificationBodyProps
 
-    await validations.validateCreatePhoneVerificationBody(params);
+    await validations.validateCreatePhoneVerificationBody(params)
 
-    const phoneAreaCode = trim(params.phoneAreaCode);
-    const phoneNumber = trim(params.phoneNumber);
-    const generate = code.generate(6);
-    const expire = parseInt(process.env.VERIFY_EXPIRE ?? "120");
+    const phoneAreaCode = trim(params.phoneAreaCode)
+    const phoneNumber = trim(params.phoneNumber)
+    const generate = code.generate(6)
+    const expire = parseInt(process.env.VERIFY_EXPIRE ?? "120")
 
-    const set = await code.set(phoneAreaCode + phoneNumber, generate, expire);
+    const set = await code.set(phoneAreaCode + phoneNumber, generate, expire)
 
     try {
-      // const api = new Netgsm({
-      //   username: process.env.NETGSM_USERNAME,
-      //   password: process.env.NETGSM_PASSWORD,
-      //   msgheader: process.env.NETGSM_MSGHEADER,
-      // });
+      const api = new Netgsm({
+        username: process.env.NETGSM_USERNAME,
+        password: process.env.NETGSM_PASSWORD,
+        msgheader: process.env.NETGSM_MSGHEADER,
+      })
 
-      // const req = await api.otp(
-      //   phoneAreaCode + phoneNumber,
-      //   params.message.replace("{code}", generate.toString())
-      // );
+      const req = await api.otp(
+        phoneAreaCode + phoneNumber,
+        params.message.replace("{code}", generate.toString())
+      )
 
       ctx.body = {
         phoneAreaCode,
         phoneNumber,
         set,
-      };
+      }
     } catch (error) {
-      throw new errors.ApplicationError(error.message);
+      throw new errors.ApplicationError(error.message)
     }
   },
 
   phoneVerification: async (ctx) => {
-    const params = ctx.request.body as PhoneVerificationBodyProps;
+    const params = ctx.request.body as PhoneVerificationBodyProps
 
-    await validations.validatePhoneVerificationBody(params);
+    await validations.validatePhoneVerificationBody(params)
 
-    const phoneAreaCode = trim(params.phoneAreaCode);
-    const phoneNumber = trim(params.phoneNumber);
+    const phoneAreaCode = trim(params.phoneAreaCode)
+    const phoneNumber = trim(params.phoneNumber)
 
-    const get = await code.get(phoneAreaCode + phoneNumber);
+    const get = await code.get(phoneAreaCode + phoneNumber)
 
     if (!get) {
-      throw new errors.NotFoundError("Not found code");
+      throw new errors.NotFoundError("Not found code")
     }
 
     if (get !== params.code) {
-      throw new errors.UnauthorizedError("The code is not correct");
+      throw new errors.UnauthorizedError("The code is not correct")
     }
 
-    const findUsers = await strapi.entityService.findMany(
-      "plugin::strapi-netgsm.user-phone",
-      {
-        filters: {
-          phoneAreaCode,
-          phoneNumber,
-        },
-        populate: {
-          user: true,
-        },
-      }
-    );
+    const findUsers = await strapi.entityService.findMany("plugin::strapi-netgsm.user-phone", {
+      filters: {
+        phoneAreaCode,
+        phoneNumber,
+      },
+      populate: {
+        user: true,
+      },
+    })
 
-    let jwt: string | undefined = undefined;
-    let user: Record<any, any> | undefined = undefined;
-    let accessKeys;
+    let jwt: string | undefined = undefined
+    let user: Record<any, any> | undefined = undefined
+    let accessKeys
 
     if (findUsers?.length) {
-      user = findUsers[0];
+      user = findUsers[0]
     } else {
-      accessKeys = await access.generateHash();
+      accessKeys = await access.generateHash()
     }
 
     if (user) {
-      jwt = strapi
-        .plugin("users-permissions")
-        .service("jwt")
-        .issue({ id: user.user.id });
+      jwt = strapi.plugin("users-permissions").service("jwt").issue({ id: user.user.id })
     }
 
-    await code.del(params.code);
+    await code.del(params.code)
 
     try {
       ctx.body = jwt
@@ -114,9 +107,9 @@ export default ({ strapi }) => ({
             jwt: jwt,
             user: user,
           }
-        : accessKeys;
+        : accessKeys
     } catch (error) {
-      throw new errors.ApplicationError(error.message);
+      throw new errors.ApplicationError(error.message)
     }
   },
-});
+})
